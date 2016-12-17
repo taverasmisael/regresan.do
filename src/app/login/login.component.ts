0001a8de96dba250e7a519b0b2ca31973532778d
TaverasMisael <macrot29@gmail.com>
2016-12-16 22:09:13 -0400

    feature: use FormBuilder instead of TDF

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  username: FormControl;
+  password: FormControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.username = new FormControl('', [ Validators.required ]);
+    this.password = new FormControl('', [ Validators.required ]);
+    this.loginForm = fb.group({
+      username: this.username,
+      password: this.password
+    });
+  }
 
   ngOnInit() {
+    console.log(`Initializing 'LoginComponent'`);
+  }
+
+  logUser() {
+    console.log(this.loginForm.value);
   }
 
 }
