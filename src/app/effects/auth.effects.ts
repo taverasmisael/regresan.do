811191aef6de1cb2a27fc3eaa0828ff4247c65e0
TaverasMisael <macrot29@gmail.com>
2016-12-19 18:20:23 -0400

    feature: Listen when SAVE_USER is dispatched and redirect to Dashboard

--- src/app/effects/auth.effects.ts
+++ src/app/effects/auth.effects.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Actions, Effect } from '@ngrx/effects';
 
@@ -14,17 +15,26 @@ import { ActionTypes, SaveUser, LoginFailure } from '../actions/auth.actions';
 
 import { UserService } from '../services/user.service'
 
-const { LOGIN_SUCCESS } = ActionTypes;
+const { LOGIN_SUCCESS, SAVE_USER, LOGIN_START } = ActionTypes;
 
 @Injectable()
 export class AuthEffects {
-  constructor(private userService: UserService, private actions$: Actions) { }
+  constructor(private userService: UserService,
+    private actions$: Actions,
+    private router: Router) { }
 
-  @Effect() saveUser$ = this.actions$
+  @Effect() loginUser$ = this.actions$
     .ofType(LOGIN_SUCCESS)
     .switchMap(action => {
       return this.userService.getUserData(action.payload)
         .map(res => new SaveUser(res))
         .catch(() => Observable.of(new LoginFailure({})))
+    });
+
+  @Effect() saveUser$ = this.actions$
+    .ofType(SAVE_USER)
+    .switchMap(action => {
+      this.router.navigate(['dashboard']);
+      return Observable.of({type: false});
     });
 }
