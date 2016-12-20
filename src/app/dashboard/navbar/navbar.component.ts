238b33d7c620619c573d1df37c6c855639cabd7c
TaverasMisael <macrot29@gmail.com>
2016-12-20 17:27:05 -0400

    feature: make big 'navbar' componet using transclusion (not recommended)

---
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
