1ebf3387d11e4719932ebc13fc02d01944a41397
TaverasMisael <macrot29@gmail.com>
2016-12-19 23:15:37 -0400

    feature: persistent .auth on localStorage

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,3 +1,7 @@
+import { compose } from '@ngrx/core/compose';
+import { combineReducers } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
 import { createReducer } from '../utilities/reducers';
 import { updateObject } from '../utilities/objects';
 import { AppState } from '../models/appstate';
@@ -8,7 +12,7 @@ const initialState: AppState = {
 }; // TODO ir agregando los demas estados en el mainstate
 
 const reducers = updateObject({}, AuthReducer()); // TODO agregar los otros reducers que se creen
-
-const RootReducer = createReducer(initialState, reducers);
+const authReducer = createReducer(initialState, reducers);
+const RootReducer = compose(localStorageSync(['auth'], true))(authReducer);
 
 export { RootReducer };
