0f0f0338e5428d937dcb4fff32268096f88d5589
TaverasMisael <macrot29@gmail.com>
2016-12-20 23:46:25 -0400

    fix: trigger upgradeAllRegistered:AfterViewInit to update mdl components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var componentHandler: any
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    componentHandler.upgradeAllRegistered();
   }
 
 }
