c4b8bf6cd78a7a0c3ee02698e43712b24c9ae0e1
TaverasMisael <macrot29@gmail.com>
2016-12-22 15:21:11 -0400

    refactor: move charts logic to DashboardOverviewComponent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
-import * as moment from 'moment';
-import { RespuestasService} from '../services/respuestas.service';
-import { makeDonughtChart} from '../utilities/respuestas';
-
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
@@ -24,10 +20,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   private Store: Observable<AppState>;
   private currentUser: Observable<User>;
   private userProfiles: Observable<UserProfile[]>
-  private today = moment();
-  private aWeekAgo = this.today.subtract(7, 'days');
-  private graphColors: string[] = ["#8BC34A", "#0D47A1", "#009688", "#F44336", "#FFEB3B", "#03A9F4"]
-  constructor(private store: Store<AppState>, private respuestas: RespuestasService) { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.Store = this.store.select<AppState>('MainStore');
@@ -38,15 +31,6 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     this.userProfiles = this.userData.map(userData => {
       return  userData && userData.currentUser ? userData.currentUser.Profiles : undefined;
     });
-    this.respuestas.getAll(this.aWeekAgo.unix(), this.today.unix())
-      .map(res => res['Preguntas'].reduce(makeDonughtChart, []))
-      .subscribe(data => {
-        Morris.Donut({
-          element: 'chartSucursales',
-          data,
-          colors: this.graphColors.sort(() => 0.5 - Math.random())
-        })
-      })
   }
 
   ngAfterViewInit() {
--- src/app/dashboard/views/overview/dashboard-overview.component.ts
+++ src/app/dashboard/views/overview/dashboard-overview.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as moment from 'moment';
+import { RespuestasService} from '../../../services/respuestas.service';
+import { makeDonughtChart} from '../../../utilities/respuestas';
+
+
 @Component({
   selector: 'dashboard-overview',
   templateUrl: './dashboard-overview.component.html',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardOverviewComponent implements OnInit {
 
-  constructor() { }
+  private today = moment();
+  private aWeekAgo = this.today.subtract(7, 'days');
+  private graphColors: string[] = ["#8BC34A", "#0D47A1", "#009688", "#F44336", "#FFEB3B", "#03A9F4"]
+  constructor(private respuestas: RespuestasService) { }
 
   ngOnInit() {
+    this.respuestas.getAll(this.aWeekAgo.unix(), this.today.unix())
+      .map(res => res['Preguntas'].reduce(makeDonughtChart, []))
+      .subscribe(data => {
+        Morris.Donut({
+          element: 'chartSucursales',
+          data,
+          colors: this.graphColors.sort(() => 0.5 - Math.random())
+        })
+      })
   }
 
 }
