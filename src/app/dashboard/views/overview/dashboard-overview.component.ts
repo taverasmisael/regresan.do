8cdfb7285a347726c9372600533c4c8b099c40aa
TaverasMisael <macrot29@gmail.com>
2016-12-24 00:10:38 -0400

    refactor: initialize graphLoading AfterViewInit

--- src/app/dashboard/views/overview/dashboard-overview.component.ts
+++ src/app/dashboard/views/overview/dashboard-overview.component.ts
@@ -1,12 +1,14 @@
 import 'morris.js/morris.min.js';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
+import { UserProfile } from '../../../models/userprofile';
 import { AppState } from '../../../models/appstate';
 import { AuthState } from '../../../models/authstate';
+
 import * as moment from 'moment';
 import { RespuestasService} from '../../../services/respuestas.service';
 import { makeDonughtChart} from '../../../utilities/respuestas';
@@ -17,9 +19,10 @@ import { makeDonughtChart} from '../../../utilities/respuestas';
   templateUrl: './dashboard-overview.component.html',
   styleUrls: ['./dashboard-overview.component.scss']
 })
-export class DashboardOverviewComponent implements OnInit {
+export class DashboardOverviewComponent implements OnInit, AfterViewInit {
 
   private AuthState: Observable<AuthState>;
+  private userProfiles: Observable<UserProfile[]>;
   private today = moment();
   private aWeekAgo = this.today.subtract(7, 'days');
   private graphColors: string[] = ["#8BC34A", "#0D47A1", "#009688", "#F44336", "#FFEB3B", "#03A9F4"]
@@ -27,6 +30,9 @@ export class DashboardOverviewComponent implements OnInit {
 
   ngOnInit() {
     this.AuthState = this.store.select<AppState>('MainStore').map(({auth}) => auth);
+    this.userProfiles = this.AuthState.map(({currentUser}) => currentUser.Profiles)
+  }
+  ngAfterViewInit() {
     this.respuestas.getAll(this.aWeekAgo.unix(), this.today.unix())
       .map(res => res['Preguntas'].reduce(makeDonughtChart, []))
       .subscribe(data => {
