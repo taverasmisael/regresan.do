6db13fe85f8f71b7b0bc7839288ee06440468ec3
TaverasMisael <macrot29@gmail.com>
2016-12-23 22:56:23 -0400

    feature: add Store and ge the Auth slice

--- src/app/dashboard/views/overview/dashboard-overview.component.ts
+++ src/app/dashboard/views/overview/dashboard-overview.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { AppState } from '../../../models/appstate';
+import { AuthState } from '../../../models/authstate';
+
 import * as moment from 'moment';
 import { RespuestasService} from '../../../services/respuestas.service';
 import { makeDonughtChart} from '../../../utilities/respuestas';
@@ -12,10 +18,13 @@ import { makeDonughtChart} from '../../../utilities/respuestas';
 })
 export class DashboardOverviewComponent implements OnInit {
 
+  private AuthState: Observable<AuthState>;
   private today = moment();
   private aWeekAgo = this.today.subtract(7, 'days');
   private graphColors: string[] = ["#8BC34A", "#0D47A1", "#009688", "#F44336", "#FFEB3B", "#03A9F4"]
-  constructor(private respuestas: RespuestasService) { }
+  constructor(private respuestas: RespuestasService, private store: Store<AppState>) {
+      this.AuthState = this.store.select<AppState>('MainStore').map(({auth}) => auth);
+  }
 
   ngOnInit() {
     this.respuestas.getAll(this.aWeekAgo.unix(), this.today.unix())
