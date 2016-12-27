668af7a43c5623ba7aeb912380062983fc44b555
TaverasMisael <macrot29@gmail.com>
2016-12-27 15:59:08 -0400

    feat: add a makePieChart function

--- src/app/utilities/respuestas.ts
+++ src/app/utilities/respuestas.ts
@@ -2,6 +2,10 @@ export function makeDonughtChart(prev = [], curr) {
   return [...prev, Object.assign({}, prev, {value: curr.Total, label: curr.Sucursal})]
 }
 
+export function makePieChart(prev = [], curr) {
+  return [...prev, Object.assign({}, prev, {value: curr.Porcentaje, label: curr.Respuesta})]
+}
+
 interface EncuestasSucursales {
   $id: string,
   Total: number,
