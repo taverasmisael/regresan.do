be115ca64524040d3ff3c8d5efc3684a13ed3531
TaverasMisael <macrot29@gmail.com>
2017-01-02 14:57:00 -0400

    feat: add select component for mdl

--- src/thirdparty.ts
+++ src/thirdparty.ts
@@ -1,5 +1,7 @@
 import 'material-design-lite/dist/material.min.js';
 import 'chart.js/src/chart.js';
+import 'mdl-select-component/mdl-selectfield.min';
+
 import * as moment from 'moment';
 import 'moment/min/locales';
 moment.locale('es-do');
