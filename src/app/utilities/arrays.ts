b99cc3b9916f2323ba84778c9d3ee38718cbe842
TaverasMisael <macrot29@gmail.com>
2017-01-05 16:20:27 -0400

    feat: add groupBy Function to the array utilities collection

--- src/app/utilities/arrays.ts
+++ src/app/utilities/arrays.ts
@@ -1,2 +1,11 @@
 export const merge = (prev, curr) => [...prev, ...curr];
 export const sum = (prev, curr) => prev + curr;
+export const groupBy = (array: any[], f: (el: any) => any[]) => {
+    let groups = {};
+    array.forEach(function (o) {
+      let group = JSON.stringify(f(o));
+      groups[group] = groups[group] || [];
+      groups[group].push(o);
+    });
+    return Object.keys(groups).map(group => groups[group])
+  }
