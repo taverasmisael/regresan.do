570d9c47c7ac525539a029cdc802ccc658234aa8
TaverasMisael <macrot29@gmail.com>
2017-01-07 14:45:41 -0400

    feat: add loading effect to the open-questions component

--- src/app/ui/answers-tabs/answers-tabs.component.ts
+++ src/app/ui/answers-tabs/answers-tabs.component.ts
@@ -9,6 +9,7 @@ export class AnswersTabsComponent implements OnInit {
   @Input() title: string;
   @Input() questions: any[];
   @Input() answers: any[];
+  @Input() loading: boolean;
 
   constructor() { }
 
