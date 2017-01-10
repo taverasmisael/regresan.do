a9005410d7a7e30c2f127e4d97f8822299a42b3f
TaverasMisael <macrot29@gmail.com>
2017-01-10 11:17:06 -0400

    feat: install dialog-polyfill

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,8 @@
 // Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
-declare var componentHandler: any
+declare var componentHandler: any;
+declare var dialogPolyfill: {
+  forceRegisterDialog: (HTMLElement) => void,
+  registerDialog: (HTMLElement) => void,
+  DialogManager: () => HTMLElement,
+};
