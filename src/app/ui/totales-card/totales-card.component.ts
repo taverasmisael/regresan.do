a1f85dc6b66c67b44f9875ecf2be5fb80ea7762d
TaverasMisael <macrot29@gmail.com>
2017-01-11 22:58:05 -0400

    feat: totales-card component initialization

---
+++ src/app/ui/totales-card/totales-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-totales-card',
+  templateUrl: './totales-card.component.html',
+  styleUrls: ['./totales-card.component.scss']
+})
+export class TotalesCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
