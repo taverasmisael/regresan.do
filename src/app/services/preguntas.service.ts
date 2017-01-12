18b86f6da8eea1e8323a7b9a0c43eaa1514f7b03
TaverasMisael <macrot29@gmail.com>
2017-01-12 14:15:00 -0400

    feat: getResumen[Sucursal] for load the totales-card

--- src/app/services/preguntas.service.ts
+++ src/app/services/preguntas.service.ts
@@ -82,4 +82,29 @@ export class PreguntasService {
       search: params
     });
   }
+
+  getResumen(query: APIRequestParams) {
+    const url = `${this.BASE_URL}/GetDatosCabecera2`;
+    const params = new URLSearchParams();
+    params.append('_startDate', query.start);
+    params.append('_endDate', query.end);
+
+    return this.api.get(url, {
+      headers: this.authHeader,
+      search: params
+    });
+  }
+
+  getResumenSucursal(query: APIRequestUser) {
+    const url = `${this.BASE_URL}/GetDatosCabecerabyProfileId2`;
+    const params = new URLSearchParams();
+    params.append('_startDate', query.start);
+    params.append('_endDate', query.end);
+    params.append('_profileId', query.profile);
+
+    return this.api.get(url, {
+      headers: this.authHeader,
+      search: params
+    });
+  }
 }
