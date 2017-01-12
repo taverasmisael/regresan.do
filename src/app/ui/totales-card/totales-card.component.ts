989633fba3117801d39ce309465e7035164d7c1b
TaverasMisael <macrot29@gmail.com>
2017-01-11 23:56:42 -0400

    feat: add input for the count and text

--- src/app/ui/totales-card/totales-card.component.ts
+++ src/app/ui/totales-card/totales-card.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-totales-card',
   templateUrl: './totales-card.component.html',
-  styleUrls: ['./totales-card.component.scss']
+  styleUrls: ['./totales-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TotalesCardComponent implements OnInit {
 
   @Input() cardType: 'total' | 'indice' | 'hoy';
+  @Input() count: number;
+  @Input() text: string;
 
   constructor() { }
 
