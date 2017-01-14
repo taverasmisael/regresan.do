dad9213f900907b24ce867bcc93854820ee7b52e
TaverasMisael <macrot29@gmail.com>
2017-01-14 01:55:01 -0400

    refact: update the 'updateItemInArray' function to work as desired

--- src/app/utilities/objects.ts
+++ src/app/utilities/objects.ts
@@ -1,7 +1,7 @@
 export const updateObject = <T, U> (oldObject: T, newValues: U): T & U => Object.assign({}, oldObject, newValues);
 
-export const updateItemInArray = <T> (array: T[], itemId: number, updateItemCallback: Function): Array<T> => {
-  return array.map(item => (item['id'] !== itemId) ? item : updateItemCallback(item));
+export const updateItemInArray = <T> (array: T[], itemId: string, updateItemCallback: Function): Array<T> => {
+  return array.map(item => item[itemId] ? updateItemCallback(item) : item);
 };
 
 export const removeArrayItem = (array, index): Array<any> => {
