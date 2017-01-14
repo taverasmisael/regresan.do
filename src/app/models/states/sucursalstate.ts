206c1415621f3a4594011989e992fd5c78d9c7c8
TaverasMisael <macrot29@gmail.com>
2017-01-14 01:54:24 -0400

    feat: add constructor. add answerCharts key

--- src/app/models/states/sucursalstate.ts
+++ src/app/models/states/sucursalstate.ts
@@ -8,6 +8,7 @@ export class SucursalState {
   closeQuestions: Pregunta[];
   openQuestions: Pregunta[];
   openAnswers: RespuestaAbierta[];
+  answerCharts: {[key: string]: any[][]};
   closeAnswers: Pregunta[];
   filters: any[];
   dates: moment.Moment[];
@@ -15,5 +16,16 @@ export class SucursalState {
   lastResult: SucursalState;
   currentAction: string;
   loading: boolean;
+
+  constructor(info?, cq?, oq?, ca?, oa?, filters?, loading?) {
+    this.info = info || new UserProfile();
+    this.closeQuestions = cq || [];
+    this.openQuestions = oq || [];
+    this.closeAnswers = ca || [];
+    this.openAnswers = oa || [];
+    this.filters = filters || [];
+    this.loading = loading || false;
+    this.answerCharts = {};
+  }
 };
 
