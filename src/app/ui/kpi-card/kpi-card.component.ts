1478ffc57ac79f490bd8a60750fc768f8e040328
TaverasMisael <macrot29@gmail.com>
2017-01-20 15:49:18 -0400

    feat: splice mainKPI from others. Add basic Markup to KPIs

--- src/app/ui/kpi-card/kpi-card.component.ts
+++ src/app/ui/kpi-card/kpi-card.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, Output, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  Output,
+  SimpleChanges,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-kpi-card',
@@ -6,15 +14,30 @@ import { Component, OnInit, Input, Output, ChangeDetectionStrategy } from '@angu
   styleUrls: ['./kpi-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class KpiCardComponent implements OnInit {
+export class KpiCardComponent implements OnInit, OnChanges {
 
   @Input() kpis: any[];
   @Input() errorText: string;
   @Input() loading: boolean;
 
+  public mainKPI: any;
+  public secondariesKPI: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.spliceKPIS(this.kpis);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['kpis'] && (changes['kpis'].previousValue !== changes['kpis'].currentValue)) {
+      this.spliceKPIS(changes['kpis'].currentValue);
+    }
+  }
+
+  spliceKPIS(kpis: any[]) {
+    this.mainKPI = this.kpis.slice(0, 1)[0];
+    this.secondariesKPI = this.kpis.slice(1);
+    console.log(this.mainKPI, this.secondariesKPI)
+  }
 }
