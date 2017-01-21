ec0a3861e50dd518bed88a45386e1aa2bb31e1fe
TaverasMisael <macrot29@gmail.com>
2017-01-21 00:59:24 -0400

    feat: implements ng2-kw-gauge

--- src/app/ui/kpi-card/kpi-card.component.ts
+++ src/app/ui/kpi-card/kpi-card.component.ts
@@ -8,6 +8,16 @@ import {
   ChangeDetectionStrategy,
 } from '@angular/core';
 
+import compare from 'just-compare';
+
+import { GaugeSegment, GaugeLabel } from 'ng2-kw-gauge';
+
+import { KPI } from '../../models/kpi';
+import { GaugeOptions } from '../../models/gauge-options';
+
+import { createGauge } from '../../utilities/gauges';
+import { gamaRegresando } from '../../utilities/colors';
+
 @Component({
   selector: 'app-kpi-card',
   templateUrl: './kpi-card.component.html',
@@ -16,27 +26,34 @@ import {
 })
 export class KpiCardComponent implements OnInit, OnChanges {
 
-  @Input() kpis: any[];
+  @Input() kpis: KPI[];
   @Input() errorText: string;
   @Input() loading: boolean;
 
-  public mainKPI: any;
-  public secondariesKPI: any[];
+  public mainKPI: GaugeOptions;
+  public secondariesKPI: GaugeOptions[];
+
+  private COLORS = gamaRegresando();
 
   constructor() { }
 
-  ngOnInit() {
-    this.spliceKPIS(this.kpis);
-  }
+  ngOnInit() { }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['kpis'] && (changes['kpis'].previousValue !== changes['kpis'].currentValue)) {
-      this.spliceKPIS(changes['kpis'].currentValue);
+    if (changes['kpis'] && changes['kpis'].currentValue.length && !compare(changes['kpis'].previousValue, changes['kpis'].currentValue)) {
+      let gauges = this.formatKPIS(<KPI[]>changes['kpis'].currentValue);
+      this.spliceKPIS(gauges);
     }
   }
 
-  spliceKPIS(kpis: any[]) {
-    this.mainKPI = this.kpis.slice(0, 1)[0];
-    this.secondariesKPI = this.kpis.slice(1);
+  spliceKPIS(kpis: GaugeOptions[]) {
+    this.mainKPI = kpis.slice(0, 1)[0];
+    this.secondariesKPI = kpis.slice(1);
+  }
+
+  formatKPIS(kpis: KPI[]) {
+    return kpis.map((kpi, index) => {
+      return createGauge({text: kpi.name, value: kpi.value, color: this.COLORS[index]})
+    })
   }
 }
