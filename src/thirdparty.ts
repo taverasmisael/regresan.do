21b6fc1f294429705186be4c13f756afba6c50b3
TaverasMisael <macrot29@gmail.com>
2017-01-24 11:30:51 -0400

    fix: Firefox/Edge dialogPolyfill Undefined

--- src/thirdparty.ts
+++ src/thirdparty.ts
@@ -1,6 +1,5 @@
 import 'material-design-lite/dist/material.min.js';
 import 'chart.js/src/chart.js';
-import 'dialog-polyfill/dialog-polyfill';
 import 'mdl-select-component/mdl-selectfield.min';
 
 import * as moment from 'moment';
