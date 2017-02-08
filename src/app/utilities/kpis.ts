32098c2436dc47c6e64eef7cfd0b6ace8239c24c
TaverasMisael <macrot29@gmail.com>
2017-02-08 01:25:34 -0400

    feat: add Kpi Utilitie
    Por ahora esta utilidad solo se encarga de hacer graficos lineales en base a unos
    KpiCamareros que son los que la API devuelve en 'GetKpisCamareros' pero en un futuro
    cualquier cosa relacionada con el manejo de KPIS se hará aquí

---
+++ src/app/utilities/kpis.ts
@@ -0,0 +1,30 @@
+import { KPICamarero } from '../models/kpi-camarero';
+
+export const createLinearCamareroKpi = (data: CamarerosKpi[]) => {
+  let labels = [];
+  const mapped = data.map((entry, index) => {
+    const mappedKpis = entry.Kpis.map(kpi => ({
+      nombre: kpi.Nombre,
+      indice: kpi.Indice
+    }));
+    labels = [...mappedKpis]
+      .reduce((prev, curr) =>
+        (labels.findIndex(el => el === curr.nombre) !== -1) ? prev : [...prev, curr.nombre], []
+      )
+    return {
+      label: entry.Session || `Anonimo ${index}`,
+      data: mappedKpis.map(kpi => kpi.indice),
+    }
+  });
+
+  return {
+    labels,
+    data: mapped
+  }
+}
+
+export interface CamarerosKpi {
+  $id: string,
+  Session: string,
+  Kpis: KPICamarero[]
+}
