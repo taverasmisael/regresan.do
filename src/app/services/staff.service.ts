e993b43814404bdd2d396131bec648ac5e6b4869
TaverasMisael <macrot29@gmail.com>
2017-02-07 23:45:02 -0400

    feat: add getKpiCamareros method

--- src/app/services/staff.service.ts
+++ src/app/services/staff.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@angular/core';
 
+import { APIRequestUser } from '../models/apiparams';
+
+import {
+  Response,
+  Headers,
+  RequestOptions,
+  RequestOptionsArgs,
+  Request,
+  URLSearchParams
+} from '@angular/http';
+
+
+import { Observable } from 'rxjs/Rx';
+
+// Auth Stuffs
+import { Store } from '@ngrx/store';
+import { AppState } from '../models/states/appstate';
+import { JWT } from '../models/jwt';
+
+import { ApiService } from './api.service';
+
 @Injectable()
 export class StaffService {
+  private BASE_URL = 'api/Respuesta';
+  private authHeader: Headers;
 
-  constructor() { }
+  constructor(private api: ApiService, store: Store<AppState>) {
+    store.select<AppState>('MainStore')
+      .distinctUntilKeyChanged('auth')
+      .pluck<JWT>('auth', 'token')
+      .subscribe(token => {
+        if (token) {
+          this.authHeader = new Headers({
+            'Authorization': `${token.token_type} ${token.access_token}`
+          });
+        } else {
+          this.authHeader = undefined;
+        }
+      });
+  }
 
+getKpisCamareros(query: APIRequestUser) {
+    const url = `${this.BASE_URL}/GetKpisCamareros`;
+    const params = new URLSearchParams();
+
+    params.append('_startDate', query.start);
+    params.append('_endDate', query.end);
+    params.append('_profileId', query.profile);
+
+    return this.api.get(url, {
+      headers: this.authHeader,
+      search: params
+    });
+  }
 }
