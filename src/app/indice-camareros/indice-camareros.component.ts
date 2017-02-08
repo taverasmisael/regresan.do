ad2cf97ce9875370df5b211f5a68a1cbdaf78038
TaverasMisael <macrot29@gmail.com>
2017-02-07 23:04:23 -0400

    feat: add optimization and polyfilling to indice-camareros

--- src/app/indice-camareros/indice-camareros.component.ts
+++ src/app/indice-camareros/indice-camareros.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ViewChild,
+  Input,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-indice-camareros',
   templateUrl: './indice-camareros.component.html',
-  styleUrls: ['./indice-camareros.component.scss']
+  styleUrls: ['./indice-camareros.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IndiceCamarerosComponent implements OnInit {
 
@@ -12,6 +20,9 @@ export class IndiceCamarerosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if (!this.indiceCamarerosDialog.nativeElement.showModal) {
+      dialogPolyfill.registerDialog(this.indiceCamarerosDialog.nativeElement);
+    }
   }
 
   showDialog() {
