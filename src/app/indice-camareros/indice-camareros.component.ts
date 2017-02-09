f02dd14fea90f414e2f54fb076b1072cdbb06323
TaverasMisael <macrot29@gmail.com>
2017-02-08 21:59:50 -0400

    feat: prevent for requesting the same data

--- src/app/indice-camareros/indice-camareros.component.ts
+++ src/app/indice-camareros/indice-camareros.component.ts
@@ -3,8 +3,12 @@ import {
   OnInit,
   ViewChild,
   Input,
-  ElementRef
+  ElementRef,
+  SimpleChanges,
+  OnChanges
 } from '@angular/core';
+
+import compare from 'just-compare';
 
 import { StaffService } from '../services/staff.service';
 
@@ -19,16 +23,17 @@ import { gamaRegresando } from '../utilities/colors';
   templateUrl: './indice-camareros.component.html',
   styleUrls: ['./indice-camareros.component.scss']
 })
-export class IndiceCamarerosComponent implements OnInit {
+export class IndiceCamarerosComponent implements OnInit, OnChanges {
   private colors: string[];
   private COLORS: string[];
+  private needsToRequest: Boolean;
 
   @Input() filter: APIRequestUser;
   @ViewChild('indiceCamarerosDialog') indiceCamarerosDialog: ElementRef;
 
   public loading: Boolean;
   public errorText: string;
-  public chartData: {labels: any[], data: any[]};
+  public chartData: { labels: any[], data: any[] };
   public chartOptions: any;
   public chartColor: ChartJsColor[];
 
@@ -38,24 +43,33 @@ export class IndiceCamarerosComponent implements OnInit {
     if (!this.indiceCamarerosDialog.nativeElement.showModal) {
       dialogPolyfill.registerDialog(this.indiceCamarerosDialog.nativeElement);
     }
-    this.chartOptions = { scales: { yAxes: [{ ticks: { beginAtZero: true } }] } };
+    this.chartOptions = {};
     this.colors = gamaRegresando().reverse();
     this.COLORS = [...this.colors].sort(() => 0.5 - Math.random());
     this.chartColor = createPalette(this.COLORS, 0);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['filter'] && !compare(changes['filter'].currentValue, changes['filter'].previousValue)) {
+      this.needsToRequest = true;
+    }
+  }
+
   showDialog() {
-    this.loading = true;
     this.indiceCamarerosDialog.nativeElement.showModal();
-    this.service.getKpisCamareros(this.filter)
-      .map(res => createLinearCamareroKpi(res['Camareros']))
-      .subscribe(
-        (res) => {
-          this.loading = false;
-          this.chartData = res;
-        } ,
-        (error) => this.handleErrors(error)
-      );
+    if (this.needsToRequest) {
+      this.loading = true;
+      this.service.getKpisCamareros(this.filter)
+        .map(res => createLinearCamareroKpi(res['Camareros']))
+        .subscribe(
+          (res) => {
+            this.loading = false;
+            this.chartData = res;
+          },
+          (error) => this.handleErrors(error),
+          () => this.needsToRequest = false
+        );
+    }
   }
 
   private handleErrors(err) {
