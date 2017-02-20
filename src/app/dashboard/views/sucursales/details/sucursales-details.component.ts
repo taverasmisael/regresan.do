eef73638ce30e438d1fd5064c8b6cd9c177fe73e
TaverasMisael <macrot29@gmail.com>
2017-02-19 23:15:18 -0400

    feat: make basic QuestionRequest and save to Store

--- src/app/dashboard/views/sucursales/details/sucursales-details.component.ts
+++ src/app/dashboard/views/sucursales/details/sucursales-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import * as moment from 'moment';
+
+import compare from 'just-compare';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
@@ -23,7 +25,12 @@ import { Pregunta } from '@models/pregunta';
 import { APIRequestUser, APIRequestRespuesta } from '@models/apiparams';
 import { UserProfile } from '@models/userprofile';
 
-import { SaveInfo } from '@actions/branch.actions';
+import {
+  SaveInfo, ResetButInfo,
+  RequestCloseQuestion, RequestCloseAnswer, RequestOpenQuestion, RequestOpenAnswer,
+  ErrorCloseQuestion, ErrorCloseAnswer, ErrorOpenQuestion, ErrorOpenAnswer,
+  SuccessCloseQuestion, SuccessCloseAnswer, SuccessOpenQuestion, SuccessOpenAnswer
+} from '@actions/branch.actions';
 
 @Component({
   selector: 'app-sucursales-details',
@@ -34,11 +41,26 @@ export class SucursalesDetailsComponent implements OnInit, AfterViewInit, OnDest
 
   public ActiveBranch: BranchState;
 
+  private FetchInformation: EventEmitter<any>;
+  private LoadCases: any;
+
   constructor(private Preguntas: PreguntasService,
     private Respuestas: RespuestasService, KPIS: KpisService,
     private Store: Store<AppState>, private Route: ActivatedRoute) { }
 
+    // Angular Lifecycle Hooks
     ngOnInit() {
+      this.LoadCases = {
+        ALL: 'ALL',
+        OPEN: 'OPEN',
+        CLOSE: 'CLOSE',
+        KPIS: 'KPIS',
+        STAFF: 'STAFF',
+        HISTORIC: 'HISTORIC'
+      };
+      this.FetchInformation = new EventEmitter();
+      this.FetchInformation.subscribe(($event) => this.onFetchInformation($event));
+
       // This update the ActiveBranch on each StoreAction
       this.Store.select('MainStore')
         .distinctUntilKeyChanged('currentSucursal')
@@ -46,18 +68,21 @@ export class SucursalesDetailsComponent implements OnInit, AfterViewInit, OnDest
           this.ActiveBranch = branch;
         });
 
+
       const profiles$ = this.Store.select('MainStore')
         .pluck<UserProfile[]>('auth', 'currentUser', 'Profiles');
 
       // Get the Route Params
-      this.Route.params.switchMap(
+      this.Route.params.distinctUntilKeyChanged('id')
+      .switchMap(
         (params: Params) =>
            profiles$.map(profiles => // Retrieve The Current Branch from the UserProfile List
             profiles.find(prof => prof.OldProfileId === +params['id']))
       ).subscribe(
         (res) =>  {
-          if (res) {
+          if (!compare(res, this.ActiveBranch.info)) { // Security Measures Prevents Infinite Loop
             this.Store.dispatch(new SaveInfo(res));
+            this.FetchInformation.emit(this.LoadCases.OPEN);
           }
         },
         (error) => console.log(error)
@@ -78,9 +103,62 @@ export class SucursalesDetailsComponent implements OnInit, AfterViewInit, OnDest
     ngOnDestroy() {
       console.log('Destroying...');
     }
+
+    // Public Methods
+
+
+    // Private Methods
+    private LoadQuestions(query: APIRequestUser) {
+      this.Store.dispatch(new RequestOpenQuestion('Cargando Preguntas Abiertas...'));
+      this.Store.dispatch(new RequestCloseQuestion('Cargando Preguntas Cerradas...'));
+
+      return this.Preguntas.getAllByProfile(query)
+      .map<Pregunta[]>(res => res['Respuestas']).toPromise()
+        .then(questions => {
+          const close = questions.filter(q => q.tipoPregunta !== 'Abierta');
+          const open = questions.filter(q => q.tipoPregunta === 'Abierta');
+
+          this.Store.dispatch(new SuccessOpenQuestion(open));
+          this.Store.dispatch(new SuccessCloseQuestion(close));
+          return questions;
+        })
+        .catch((err) => {
+          this.Store.dispatch(new ErrorOpenQuestion(err));
+          this.Store.dispatch(new ErrorCloseQuestion(err));
+        });
+    }
+
+    private LoadOpenAnswers(questions: Pregunta[]) {
+      console.log(questions);
+    }
+    private onFetchInformation(event) {
+      const mockQuery = {
+        start: moment().subtract(7, 'days').unix().toString(),
+        end: moment().unix().toString(),
+        profile: this.ActiveBranch.info.OldProfileId.toString()
+      };
+      switch (event) {
+        case this.LoadCases.OPEN:
+          console.log('Calling Open Data...');
+          this.LoadQuestions(mockQuery)
+            .then(this.LoadOpenAnswers);
+          break;
+        case this.LoadCases.CLOSE:
+          console.log('Calling Close Data...'); // TODO: Implement
+          break;
+        case this.LoadCases.KPIS:
+          console.log('Calling KPIS...'); // TODO: Implement
+          break;
+        case this.LoadCases.STAFF:
+          console.log('Calling STAFF...'); // TODO: Implement
+          break;
+        case this.LoadCases.HISTORIC:
+          console.log('Calling Historic...'); // TODO: Implement
+          break;
+        default:
+          this.Store.dispatch(new ResetButInfo());
+          console.log('Calling All'); // Todo: Implmenet
+          break;
+      }
+    }
 }
-
-
-interface LoadAnswerParams {
-  preguntas: Pregunta[], query: APIRequestUser
-};
