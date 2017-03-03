c220dbd55ef6860cebfa4ff975ecabd30cf33bea
TaverasMisael <macrot29@gmail.com>
2017-03-03 11:41:43 -0400

    refact: rename properties according to API

--- src/app/models/answer.open.ts
+++ src/app/models/answer.open.ts
@@ -1,15 +1,20 @@
 export class OpenAnswer {
-  public fecha: string
-  public porcentaje: string
-  public pregunta: string
-  public respuesta: string
-  public sesion: string
+  public $id: string
+  public Total: string
+  public Porcentaje: string
+  public Respuesta: string
+  public Pregunta: string
+  public Fecha: string
+  public Sesion: string
 
-  constructor(fecha: string, porcentaje: string, pregunta: string, respuesta: string, sesion: string) {
-    this.fecha = fecha;
-    this.porcentaje = porcentaje;
-    this.pregunta = respuesta;
-    this.respuesta = respuesta;
-    this.sesion = sesion;
+  constructor(id: string, Total: string, Porcentaje: string,
+    Respuesta: string, Pregunta: string, Fecha: string, Sesion: string) {
+    this.$id = id;
+    this.Total = Total;
+    this.Porcentaje = Porcentaje;
+    this.Respuesta = Respuesta;
+    this.Pregunta = Pregunta;
+    this.Fecha = Fecha;
+    this.Sesion = Sesion;
   }
 }
