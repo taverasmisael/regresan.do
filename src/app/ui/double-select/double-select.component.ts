362200844f1ec9b292424d94b93337dc11691234
TaverasMisael <macrot29@gmail.com>
2017-07-07 01:10:49 -0400

    feat: create relation between options and currentSecondary complete markup

--- src/app/ui/double-select/double-select.component.ts
+++ src/app/ui/double-select/double-select.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild
+} from '@angular/core'
 
 @Component({
   selector: 'app-double-select',
   templateUrl: './double-select.component.html',
-  styleUrls: ['./double-select.component.scss']
+  styleUrls: ['./double-select.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DoubleSelectComponent implements OnInit {
+  @ViewChild('primarySelect') primarySelect: ElementRef
+  @Input() primaryLabel: string
+  @Input() secondaryLabel: string
+  @Input() options: any
 
-  constructor() { }
+  public currentSecondary = []
 
-  ngOnInit() {
+  constructor() {}
+
+  public ngOnInit() {}
+
+  public onPrimaryChange($event) {
+
+    this.currentSecondary = this.options.find(o => Number(o.value) === Number(this.primarySelect.nativeElement.value)).children
   }
-
 }
