0c0e553944518b6847562d4bd4154a7ec53c228a
TaverasMisael <macrot29@gmail.com>
2017-07-12 21:45:51 -0400

    feat: add utility to make "rest" object
    The functionality was taken compiling in babel REPL an object rest operation. See ES7 Object Rest

--- src/app/utilities/objects.ts
+++ src/app/utilities/objects.ts
@@ -1,13 +1,25 @@
-export const updateObject = <T, U> (oldObject: T, newValues: U): T & U => Object.assign({}, oldObject, newValues);
+export const updateObject = <T, U>(oldObject: T, newValues: U): T & U =>
+  Object.assign({}, oldObject, newValues)
 
-export const updateItemInArray = <T> (array: T[], itemId: string, updateItemCallback: Function): Array<T> => {
-  return array.map(item => item[itemId] ? updateItemCallback(item) : item);
-};
+export const updateItemInArray = <T>(
+  array: T[],
+  itemId: string,
+  updateItemCallback: Function
+): Array<T> => {
+  return array.map(item => (item[itemId] ? updateItemCallback(item) : item))
+}
 
 export const removeArrayItem = (array, index): Array<any> => {
-  const length = array.length;
-  return [
-    ...array.slice(0, index),
-    ...array.slice(index + 1, length)
-  ];
-};
+  const length = array.length
+  return [...array.slice(0, index), ...array.slice(index + 1, length)]
+}
+
+export const objectRest = (obj: any, keys: Array<string>) => {
+  let target = {}
+  for (let i in obj) {
+    if (keys.indexOf(i) >= 0) { continue }
+    if (!Object.prototype.hasOwnProperty.call(obj, i)) { continue }
+    target[i] = obj[i]
+  }
+  return target
+}
