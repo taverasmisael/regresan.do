323d95ec4b3b8c5f9dbad101d0b5840bd997b151
TaverasMisael <macrot29@gmail.com>
2017-08-20 01:55:12 -0400

    fix: Effects and Actions in app

--- src/app/actions/branch.actions.ts
+++ src/app/actions/branch.actions.ts
@@ -1,179 +1,177 @@
-import { Action } from '@ngrx/store';
-import * as ACTIONS from './branch.types';
+import { Action } from '@ngrx/store'
+import * as ACTIONS from './branch.types'
 
-import { ActionEnhanced } from '@models/action.enhanced';
-import { UserProfile} from '@models/userprofile';
-import { RequestError } from '@models/request-error';
-import { Pregunta } from '@models/pregunta';
-import { CloseAnswer } from '@models/answer.close';
-import { OpenAnswer } from '@models/answer.open';
-import { KPI } from '@models/kpi';
-import { StaffRanking } from '@models/staff-ranking';
-import { HistoricEntry } from '@models/historic-entry';
-import { APIRequestParams, APIRequestUser, APIRequestRespuesta } from '@models/apiparams';
-import QuestionFilter from '@models/filter-question';
+import { ActionEnhanced } from '@models/action.enhanced'
+import { UserProfile } from '@models/userprofile'
+import { RequestError } from '@models/request-error'
+import { Pregunta } from '@models/pregunta'
+import { CloseAnswer } from '@models/answer.close'
+import { OpenAnswer } from '@models/answer.open'
+import { KPI } from '@models/kpi'
+import { StaffRanking } from '@models/staff-ranking'
+import { HistoricEntry } from '@models/historic-entry'
+import { APIRequestParams, APIRequestUser, APIRequestRespuesta } from '@models/apiparams'
+import QuestionFilter from '@models/filter-question'
 
-const getSectionName = (type: string) => type.split('_')[1];
+const getSectionName = (type: string) => type.split('_')[1]
 
 // Requests
 export class RequestQuestions implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_QUESTIONS_R;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_QUESTIONS_R
+  public section = getSectionName(this.type)
 
-  constructor(public payload: APIRequestUser, public message: string) { }
+  constructor(public payload: APIRequestUser, public message: string) {}
 }
 
 export class RequestOpenAnswer implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_AOPEN_R;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_AOPEN_R
+  public section = getSectionName(this.type)
 
-  constructor(public payload: APIRequestRespuesta, message: string) { }
+  constructor(public payload: APIRequestRespuesta, message: string) {}
 }
 
 export class RequestCloseAnswer implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_ACLOSE_R;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_ACLOSE_R
+  public section = getSectionName(this.type)
 
-  constructor(public payload: APIRequestRespuesta, message: string) { }
+  constructor(public payload: APIRequestRespuesta, message: string) {}
 }
 
 export class RequestKPI implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_KPI_R;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_KPI_R
+  public section = getSectionName(this.type)
 
-  constructor(public payload: APIRequestUser, message: string) { }
+  constructor(public payload: APIRequestUser, message: string) {}
 }
 
 export class RequestStaffRanking implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_STAFF_RANKING_R;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_STAFF_RANKING_R
+  public section = getSectionName(this.type)
 
-  constructor(public payload: APIRequestUser, message: string) { }
+  constructor(public payload: APIRequestUser, message: string) {}
 }
 
 export class RequestHistoric implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_HISTORIC_R;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_HISTORIC_R
+  public section = getSectionName(this.type)
 
-  constructor(public payload: APIRequestParams, message: string) { }
+  constructor(public payload: APIRequestParams, message: string) {}
 }
 
 // Errors
 export class ErrorQuestions implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_QUESTIONS_E;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_QUESTIONS_E
+  public section = getSectionName(this.type)
 
-  constructor(public payload: RequestError) { }
+  constructor(public payload: RequestError) {}
 }
 
 export class ErrorOpenAnswer implements Action {
-  public type = ACTIONS.BRANCH_REQ_AOPEN_E;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_AOPEN_E
+  public section = getSectionName(this.type)
 
-  constructor(public payload: RequestError) { }
+  constructor(public payload: RequestError) {}
 }
 
 export class ErrorCloseAnswer implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_ACLOSE_E;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_ACLOSE_E
+  public section = getSectionName(this.type)
 
-  constructor(public payload: RequestError) { }
+  constructor(public payload: RequestError) {}
 }
 
 export class ErrorKPI implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_KPI_E;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_KPI_E
+  public section = getSectionName(this.type)
 
-  constructor(public payload: RequestError) { }
+  constructor(public payload: RequestError) {}
 }
 
 export class ErrorStaffRanking implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_STAFF_RANKING_E;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_STAFF_RANKING_E
+  public section = getSectionName(this.type)
 
-  constructor(public payload: RequestError) { }
+  constructor(public payload: RequestError) {}
 }
 
 export class ErrorHistoric implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_HISTORIC_E;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_HISTORIC_E
+  public section = getSectionName(this.type)
 
-  constructor(public payload: RequestError) { }
+  constructor(public payload: RequestError) {}
 }
-
 
 // Success
 export class SuccessQuestions implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_QUESTIONS_S;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_QUESTIONS_S
+  public section = getSectionName(this.type)
 
-  constructor(public payload: {close: Pregunta[], open: Pregunta[]}) { }
+  constructor(public payload: { close: Pregunta[]; open: Pregunta[] }) {}
 }
 
 export class SuccessOpenAnswer implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_AOPEN_S;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_AOPEN_S
+  public section = getSectionName(this.type)
 
-  constructor(public payload: {answer: OpenAnswer[], question: string}) { }
+  constructor(public payload: { answer: OpenAnswer[]; question: string }) {}
 }
 
 export class SuccessCloseAnswer implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_ACLOSE_S;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_ACLOSE_S
+  public section = getSectionName(this.type)
 
-  constructor(public payload: {answer: CloseAnswer[], question: string}) { }
+  constructor(public payload: { answer: CloseAnswer[]; question: string }) {}
 }
 
 export class SuccessKPI implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_KPI_S;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_KPI_S
+  public section = getSectionName(this.type)
 
-  constructor(public payload: KPI[]) { }
+  constructor(public payload: KPI[]) {}
 }
 
 export class SuccessStaffRanking implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_STAFF_RANKING_S;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_STAFF_RANKING_S
+  public section = getSectionName(this.type)
 
-  constructor(public payload: StaffRanking[]) { }
+  constructor(public payload: StaffRanking[]) {}
 }
 
 export class SuccessHistoric implements ActionEnhanced {
-  public type = ACTIONS.BRANCH_REQ_HISTORIC_S;
-  public section = getSectionName(this.type);
+  public type = ACTIONS.BRANCH_REQ_HISTORIC_S
+  public section = getSectionName(this.type)
 
-  constructor(public payload: HistoricEntry[]) { }
+  constructor(public payload: HistoricEntry[]) {}
 }
-
 
 // Resets & Saves
 export class ResetAll implements Action {
-  public type = ACTIONS.BRANCH_RESET_ALL;
+  public type = ACTIONS.BRANCH_RESET_ALL
 
-  constructor(public payload?: any) { }
+  constructor(public payload?: any) {}
 }
 
 export class ResetButInfo implements Action {
-  public type = ACTIONS.BRANCH_RESET_BUT_INFO;
+  public type = ACTIONS.BRANCH_RESET_BUT_INFO
 
-  constructor(public payload?: any) { }
+  constructor(public payload?: any) {}
 }
 
 export class SaveInfo implements Action {
-  public type = ACTIONS.BRANCH_INFO_SAVE;
+  public type = ACTIONS.BRANCH_INFO_SAVE
 
-  constructor(public payload: UserProfile) { }
+  constructor(public payload: UserProfile) {}
 }
 
 // Filters and Queries
 export class ApplyCurrentQuery implements Action {
-  public type = ACTIONS.BRANCH_APPLY_CURRENT_QUERY;
+  public type = ACTIONS.BRANCH_APPLY_CURRENT_QUERY
 
-  constructor(public payload: QuestionFilter) { }
+  constructor(public payload: QuestionFilter) {}
 }
 
 export class SaveCurrentQuery implements Action {
-  public type = ACTIONS.BRANCH_SAVE_CURRENT_QUERY;
+  public type = ACTIONS.BRANCH_SAVE_CURRENT_QUERY
 
-  constructor(public payload: APIRequestUser) { }
+  constructor(public payload: APIRequestUser) {}
 }
--- src/app/effects/branch.effects.ts
+++ src/app/effects/branch.effects.ts
@@ -1,106 +1,138 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
-import { Actions, Effect } from '@ngrx/effects';
+import { Store } from '@ngrx/store'
+import { Observable } from 'rxjs/Observable'
+import { Actions, Effect } from '@ngrx/effects'
 
-import { AppState } from '@models/states/appstate';
-import { APIRequestParams, APIRequestUser } from '@models/apiparams';
-import { UserProfile } from '@models/userprofile';
-import { CloseAnswer } from '@models/answer.close';
-import { BranchState } from '@models/states/branch';
-import { HistoricEntry } from '@models/historic-entry';
-import { KPI } from '@models/kpi';
-import { OpenAnswer } from '@models/answer.open';
-import { Pregunta } from '@models/pregunta';
-import { StaffRanking } from '@models/staff-ranking';
+import { AppState } from '@models/states/appstate'
+import { APIRequestParams, APIRequestUser } from '@models/apiparams'
+import { UserProfile } from '@models/userprofile'
+import { CloseAnswer } from '@models/answer.close'
+import { BranchState } from '@models/states/branch'
+import { HistoricEntry } from '@models/historic-entry'
+import { KPI } from '@models/kpi'
+import { OpenAnswer } from '@models/answer.open'
+import { Pregunta } from '@models/pregunta'
+import { StaffRanking } from '@models/staff-ranking'
 
-import { KpisService } from '@services/kpis.service';
-import { PreguntasService } from '@services/preguntas.service';
-import { RespuestasService } from '@services/respuestas.service';
-import { StaffService } from '@services/staff.service';
+import { KpisService } from '@services/kpis.service'
+import { PreguntasService } from '@services/preguntas.service'
+import { RespuestasService } from '@services/respuestas.service'
+import { StaffService } from '@services/staff.service'
 
-import { sortResDate } from '@utilities/arrays';
+import { sortResDate } from '@utilities/arrays'
 
-import * as ACTIONS from '@actions/branch.types';
-import { ActionTypes as AuthTypes } from '@actions/auth.actions';
+import * as ACTIONS from '@actions/branch.types'
+import { ActionTypes as AuthTypes } from '@actions/auth.actions'
 
 import {
-  ErrorCloseAnswer, ErrorHistoric, ErrorKPI, ErrorOpenAnswer, ErrorQuestions, ErrorStaffRanking,
-  SuccessCloseAnswer, SuccessHistoric, SuccessKPI, SuccessOpenAnswer, SuccessQuestions, SuccessStaffRanking,
+  ErrorCloseAnswer,
+  ErrorHistoric,
+  ErrorKPI,
+  ErrorOpenAnswer,
+  ErrorQuestions,
+  ErrorStaffRanking,
+  SuccessCloseAnswer,
+  SuccessHistoric,
+  SuccessKPI,
+  SuccessOpenAnswer,
+  SuccessQuestions,
+  SuccessStaffRanking,
   SaveCurrentQuery
-} from '@actions/branch.actions';
+} from '@actions/branch.actions'
 
 @Injectable()
 export class BranchEffects {
-  @Effect() requestCA$ = this.actions$
+  @Effect()
+  requestCA$ = this.actions$
     .ofType(ACTIONS.BRANCH_REQ_ACLOSE_R)
-    .map(action => action.payload)
+    .map(action => action['payload'])
     .mergeMap(payload => {
-      return this.respuestasService.getFromProfile(payload)
-        .map<CloseAnswer[]>(res => res['RespuestasPreguntas']) // return only the real data
-        .map(answer => new SuccessCloseAnswer({answer: answer, question: payload.pregunta}))
-        .catch(err => this.HandleError(err, ErrorCloseAnswer));
-    });
-
-  @Effect() requestHistoric$ = this.actions$
-    .ofType(ACTIONS.BRANCH_REQ_HISTORIC_R)
-    .map(action => action.payload)
-    .switchMap(payload => {
-      return this.preguntasService.getTotalPorDia(payload)
-        .map<HistoricEntry[]>(res => res['Encuestas']['TotalesxSucursalxDia'].sort(sortResDate))
-        .map(entries => entries.filter(entry => entry.Sucursal === this.currentBranch.Title)) // return only data of currentBranch
-        .map(entries => new SuccessHistoric(entries))
-        .catch(err => this.HandleError(err, ErrorHistoric));
-    });
-
-  @Effect() requestKPI$ = this.actions$
-    .ofType(ACTIONS.BRANCH_REQ_KPI_R)
-    .map(action => action.payload)
-    .switchMap(payload => {
-      return this.kpiService.getFromProfile(payload)
-        .map<KPI[]>(res => res['Kpis'])
-        .map(kpis => new SuccessKPI(kpis))
-        .catch(err => this.HandleError(err, ErrorKPI));
-    });
-
-  @Effect() requestOA$ = this.actions$
-    .ofType(ACTIONS.BRANCH_REQ_AOPEN_R)
-    .map(action => action.payload)
-    .mergeMap(payload => {
-      return this.respuestasService.getAbiertasFromProfile(payload)
-        .map<OpenAnswer[]>(res => res['RespuestasPreguntas'])
-        .map(answer => new SuccessOpenAnswer({answer: answer, question: payload.pregunta}))
-        .catch(err => this.HandleError(err, ErrorOpenAnswer));
+      return this.respuestasService
+        .getFromProfile(payload)
+        .map(res => res['RespuestasPreguntas']) // return only the real data
+        .map(
+          (answer: CloseAnswer[]) =>
+            new SuccessCloseAnswer({ answer: answer, question: payload.pregunta })
+        )
+        .catch(err => this.HandleError(err, ErrorCloseAnswer))
     })
 
-  @Effect() requestQS$ = this.actions$
+  @Effect()
+  requestHistoric$ = this.actions$
+    .ofType(ACTIONS.BRANCH_REQ_HISTORIC_R)
+    .map(action => action['payload'])
+    .switchMap(payload => {
+      return this.preguntasService
+        .getTotalPorDia(payload)
+        .map(res => res['Encuestas']['TotalesxSucursalxDia'].sort(sortResDate))
+        .map((entries: HistoricEntry[]) =>
+          entries.filter(entry => entry.Sucursal === this.currentBranch.Title)
+        ) // return only data of currentBranch
+        .map((entries: HistoricEntry[]) => new SuccessHistoric(entries))
+        .catch(err => this.HandleError(err, ErrorHistoric))
+    })
+
+  @Effect()
+  requestKPI$ = this.actions$
+    .ofType(ACTIONS.BRANCH_REQ_KPI_R)
+    .map(action => action['payload'])
+    .switchMap(payload => {
+      return this.kpiService
+        .getFromProfile(payload)
+        .map(res => res['Kpis'])
+        .map((kpis: KPI[]) => new SuccessKPI(kpis))
+        .catch(err => this.HandleError(err, ErrorKPI))
+    })
+
+  @Effect()
+  requestOA$ = this.actions$
+    .ofType(ACTIONS.BRANCH_REQ_AOPEN_R)
+    .map(action => action['payload'])
+    .mergeMap(payload => {
+      return this.respuestasService
+        .getAbiertasFromProfile(payload)
+        .map(res => res['RespuestasPreguntas'])
+        .map(
+          (answer: OpenAnswer[]) =>
+            new SuccessOpenAnswer({ answer: answer, question: payload.pregunta })
+        )
+        .catch(err => this.HandleError(err, ErrorOpenAnswer))
+    })
+
+  @Effect()
+  requestQS$ = this.actions$
     .ofType(ACTIONS.BRANCH_REQ_QUESTIONS_R)
-    .map(action => action.payload)
-    .switchMap(payload => { // Trying Some Different RXJS operator
-      return this.preguntasService.getAllByProfile(payload)
-        .map<Pregunta[]>(res => res['Respuestas']) // return only the real data
-        .map(questions => {
-          const close = questions.filter(q => q.tipoPregunta !== 'Abierta');
-          const open = questions.filter(q => q.tipoPregunta === 'Abierta');
+    .map(action => action['payload'])
+    .switchMap(payload => {
+      // Trying Some Different RXJS operator
+      return this.preguntasService
+        .getAllByProfile(payload)
+        .map(res => res['Respuestas']) // return only the real data
+        .map((questions: Pregunta[]) => {
+          const close = questions.filter(q => q.tipoPregunta !== 'Abierta')
+          const open = questions.filter(q => q.tipoPregunta === 'Abierta')
           return new SuccessQuestions({ close, open })
         })
-        .catch(err => this.HandleError(err, ErrorQuestions));
-    });
+        .catch(err => this.HandleError(err, ErrorQuestions))
+    })
 
-  @Effect() requestStaff$ = this.actions$
+  @Effect()
+  requestStaff$ = this.actions$
     .ofType(ACTIONS.BRANCH_REQ_STAFF_RANKING_R)
-    .map(action => action.payload)
+    .map(action => action['payload'])
     .switchMap(payload => {
-      return this.preguntasService.getRankingCamareros(payload)
-        .map<StaffRanking[]>(res => res['RankingCamareros'].sort((prev, curr) => prev.Total > curr.Total))
-        .map(ranking => new SuccessStaffRanking(ranking))
-        .catch(err => this.HandleError(err, ErrorStaffRanking));
-    });
+      return this.preguntasService
+        .getRankingCamareros(payload)
+        .map(res => res['RankingCamareros'].sort((prev, curr) => prev.Total > curr.Total))
+        .map((ranking: StaffRanking[]) => new SuccessStaffRanking(ranking))
+        .catch(err => this.HandleError(err, ErrorStaffRanking))
+    })
 
-  @Effect() applyQuery$ = this.actions$
+  @Effect()
+  applyQuery$ = this.actions$
     .ofType(ACTIONS.BRANCH_APPLY_CURRENT_QUERY)
-    .map(action => action.payload)
+    .map(action => action['payload'])
     .map(payload => ({
       profile: this.currentBranch.OldProfileId.toString(),
       start: payload.start,
@@ -110,25 +142,27 @@ export class BranchEffects {
       return Observable.of(new SaveCurrentQuery(query))
     })
 
-  public currentBranch: UserProfile;
-  constructor(private actions$: Actions,
+  public currentBranch: UserProfile
+  constructor(
+    private actions$: Actions,
     private kpiService: KpisService,
     private preguntasService: PreguntasService,
     private respuestasService: RespuestasService,
     private staffService: StaffService,
-    private store: Store<AppState>) {
-      this.store.select('MainStore')
-        .pluck('currentBranch')
-        .distinctUntilKeyChanged('info')
-        .pluck<UserProfile>('info')
-        .subscribe(val =>  this.currentBranch = val);
-    }
+    private store: Store<AppState>
+  ) {
+    this.store
+      .select('currentBranch')
+      .distinctUntilKeyChanged('info')
+      .pluck('info')
+      .subscribe((val: UserProfile) => (this.currentBranch = val))
+  }
 
   private HandleError(error: any, Action: any): Observable<any> {
     if (error.status === 401) {
-      return Observable.of({ type: AuthTypes.LOGOUT_START });
+      return Observable.of({ type: AuthTypes.LOGOUT_START })
     } else {
-      return Observable.of(new Action(error));
+      return Observable.of(new Action(error))
     }
   }
 }
